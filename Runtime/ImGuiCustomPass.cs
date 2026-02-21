31cef9620701548eb32152857dbbb1ca02baa9d0
CanTalat-Yakan <can.talat.yakan@gmail.com>
2026-02-21 22:20:14 +0100

    ⠀⠀⠀⠀⠀

--- Runtime/ImGuiCustomPass.cs
+++ Runtime/ImGuiCustomPass.cs
@@ -6,27 +6,25 @@ using UnityEngine.Rendering.HighDefinition;
 
 namespace UnityEssentials
 {
-    /// <summary>
-    /// Minimal HDRP <see cref="CustomPass"/> that forwards rendering to a referenced <see cref="ImGuiHost"/>.
-    /// </summary>
     [Serializable]
     internal sealed class ImGuiCustomPass : CustomPass
     {
         public bool ShowDemoWindow = true;
-        
+
         protected override void Execute(CustomPassContext ctx)
         {
             var cam = ctx.hdCamera?.camera;
             var host = ImGuiHost.Instance;
+
             if (host == null || cam == null)
                 return;
-            
+
             if (cam.cameraType == CameraType.SceneView)
                 return;
 
-            if(ShowDemoWindow)
+            if (ShowDemoWindow)
                 ImGui.ShowDemoWindow();
-            
+
             CoreUtils.SetRenderTarget(ctx.cmd, ctx.cameraColorBuffer);
             host.Render(ctx.cmd, cam);
         }
